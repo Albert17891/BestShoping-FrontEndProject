3e35de1e31d0736e48cdd66d9801a96deed3b310
Albert <user@WINDOWS-O7N9M38>
2023-02-16 16:37:26 +0400

    add new report link

--- src/app/product-report/product-report.component.ts
+++ src/app/product-report/product-report.component.ts
@@ -21,6 +21,10 @@ export class ProductReportComponent {
 
   }
 
+  topTenSeller(){
+     this.productReportService.topTenSeller();
+  }
+
   topTenUser(){
           this.productReportService.GetTopTenUsers();
   }
