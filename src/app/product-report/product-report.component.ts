878ac9e2a16f2be08abc08c44ce60cbac914dcfe
Albert <user@WINDOWS-O7N9M38>
2023-02-18 18:36:32 +0400

    add transaction reports

--- src/app/product-report/product-report.component.ts
+++ src/app/product-report/product-report.component.ts
@@ -17,8 +17,8 @@ export class ProductReportComponent {
         this.productReportService.topTen();       
   }
 
-  AveragePrice(){
-
+ getTransaction(){
+      this.productReportService.GetTransaction();
   }
 
   topTenSeller(){
